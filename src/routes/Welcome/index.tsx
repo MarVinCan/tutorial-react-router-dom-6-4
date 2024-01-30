4df1dcc49610a431389c7c555e0d490335450a89
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2024-01-30 10:03:06 -0300

    Index routes

---
+++ src/routes/Welcome/index.tsx
@@ -0,0 +1,7 @@
+export default function Welcome(){
+    return(
+        <main style={{ padding: "1rem" }}>
+              <h1 style={{fontSize: '30px', color: 'red' }}>Bem vindo</h1>
+            </main>
+    );
+}
